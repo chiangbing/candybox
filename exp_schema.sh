c3037e821134775decc6fdf06a3486f364718c4c
Clay Chiang <chiangbing@gmail.com>
2013-01-07 02:02:54 +0800

    unify code style

--- exp_schema.sh
+++ exp_schema.sh
@@ -1,13 +1,34 @@
 #!/usr/bin/env bash
 
-# get list from table names
-tables=( $(echo 'list' | hbase shell | sed -n '/^TABLE/,/row(s)/p' | grep -v -E '(^TABLE|row\(s\))') )
+get_tables() {
+    if [ -z "$1" ]; then
+        echo 'list' | hbase shell | sed -n '/^TABLE/,/row(s)/p' | grep -v -E '(^TABLE|row\(s\))'
+    else
+        cat $1
+    fi
+}
 
-# output create table statement for each table
-for tab in ${tables[@]}; do
-  echo -n "create '$tab',"
-  echo "describe '$tab'" | hbase shell | \
-    sed -n '/^DESC/,/row(s)/p' | grep -v -E '(^DESC|row\(s\))' | cut -b -107 | tr -d '\n ' | \
-    sed 's/.*FAMILIES=>\[\(.*\)\]}$/\1/'
-  echo
-done
+
+# main
+if [ "$BASH_SOURCE" == "$0" ]; then
+    if [ -z "$1" ]; then
+        echo "$0 <outputdir> [tablelist]" >&2
+        exit 1
+    fi
+
+    output_dir=$1
+    shift 1
+    table_list=$1
+
+    # get list from table names
+    tables=( $(get_tables $table_list) )
+
+    # output create table statement for each table
+    for tab in ${tables[@]}; do
+      echo -n "create '$tab',"
+      echo "describe '$tab'" | hbase shell | \
+        sed -n '/^DESC/,/row(s)/p' | grep -v -E '(^DESC|row\(s\))' | cut -b -107 | tr -d '\n ' | \
+        sed 's/.*FAMILIES=>\[\(.*\)\]}$/\1/'
+      echo
+    done
+fi
--- imp_data.sh
+++ imp_data.sh
@@ -11,20 +11,24 @@ get_tables() {
     fi
 }
 
-if [ -z "$1" ]; then
-    echo "$0 <inputdir> [tablelist]" >&2
-    exit 1
+
+# main
+if [ "$BASH_SOURCE" == "$0" ]; then
+    if [ -z "$1" ]; then
+        echo "$0 <inputdir> [tablelist]" >&2
+        exit 1
+    fi
+
+    input_dir=$1
+    shift 1
+    table_list=$1
+
+    # get list from table names
+    tables=( $(get_tables $table_list) )
+
+    for tab in ${tables[@]}; do
+        echo "Start importing $tab at "$(date "+%Y-%m-%d %H:%M:%S")"..."
+        hbase org.apache.hadoop.hbase.mapreduce.Import "$tab" "$input_dir"/"$tab"
+        echo "End importing $tab at"$(date "+%Y-%m-%d %H:%M:%S")"."
+    done
 fi
-
-input_dir=$1
-shift 1
-table_list=$1
-
-# get list from table names
-tables=( $(get_tables $table_list) )
-
-for tab in ${tables[@]}; do
-    echo "Start importing $tab at "$(date "+%Y-%m-%d %H:%M:%S")"..."
-    hbase org.apache.hadoop.hbase.mapreduce.Import "$tab" "$input_dir"/"$tab"
-    echo "End importing $tab at"$(date "+%Y-%m-%d %H:%M:%S")"."
-done
